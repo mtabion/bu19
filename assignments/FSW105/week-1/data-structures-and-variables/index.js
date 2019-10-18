f0eecf5b8781200d90fe6abb399f38459dca1dcd
mtabion <melvin.tabion@bryanuniversity.edu>
2019-10-17 20:50:48 -0700

    new file

---
+++ assignments/FSW105/week-1/data-structures-and-variables/index.js
@@ -0,0 +1,11 @@
+var shopper = {
+    firstName: "Melvin",
+    lastName: "Tabion",
+    isHealthy: true,
+    currentItems: 0,
+    fullName: function() {
+       console.log(this.firstName + " " + this.lastName)
+    }
+}
+
+console.log(shopper)
