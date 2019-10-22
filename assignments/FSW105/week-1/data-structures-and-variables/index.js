5aeb1011a5db055f88211a08f28115f397b99a74
mtabion <melvin.tabion@bryanuniversity.edu>
2019-10-21 18:18:56 -0700

    made corrections

--- assignments/FSW105/week-1/data-structures-and-variables/index.js
+++ assignments/FSW105/week-1/data-structures-and-variables/index.js
@@ -4,10 +4,10 @@ var shopper = {
     isHealthy: true,
     currentItems: 0,
     groceryCart: ["Steak", "Shrimp", "Asparagus", "Pasta Noodles", "Lemons", "Tomatoe Sauce"],
-    fullName: function() {
-        console.log(this.firstName + " " + this.lastName)
-     }
-}
-
-
-console.log(shopper)
+    fullName: function () {
+      console.log(this.firstName + " " + this.lastName);
+      console.log(shopper.firstName + " " + shopper.lastName);
+    }
+};
+console.log(shopper);
+shopper.fullName();
