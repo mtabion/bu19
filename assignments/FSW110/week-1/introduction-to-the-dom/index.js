6ea0e05b6b87488f50ad2732367396ac5452fdcf
mtabion <melvin.tabion@bryanuniversity.edu>
2019-10-21 20:52:26 -0700

    redo assignment

--- assignments/FSW110/week-1/introduction-to-the-dom/index.js
+++ assignments/FSW110/week-1/introduction-to-the-dom/index.js
@@ -1,1 +1,8 @@
-var 
+
+console.log("webpage is up and running");
+// Changed the header by adding Big Kahuna's
+var header = document.getElementById("header");
+header.innerHTML = "Big Kahuna's Pineapple Market";
+// I changed the original paragraph to Lorem ipsum for class purposes.
+var text = document.getElementById("paragraph");
+text.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
