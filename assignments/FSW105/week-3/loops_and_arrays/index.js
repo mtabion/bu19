c7a791072f12398764dffd07c2d1e11314232b7f
chieftain <chieftain@iMac.local>
2019-11-04 21:15:16 -0800

    submit new assignment

--- assignments/FSW105/week-3/loops_and_arrays/index.js
+++ assignments/FSW105/week-3/loops_and_arrays/index.js
@@ -1,11 +1,11 @@
-//Loop through the following array and count how many "computers" there are. Log the final count:
+//Loop through the following array and count how many "computers" there are. Log the final count:
 var officeItems = ["stapler", "monitor", "computer", "desk", "lamp",
  "computer", "lamp", "stapler", "computer", "computer"];
 var result = officeItems.filter(i => i === "computer").length;
 console.log("Number of Computers: " + result);
 
-//Loop through the following array and log to the console "old enough" if
-// they are 18 or older, and "not old enough" if they aren't 18.
+//Loop through the following array and log to the console "old enough" if
+// they are 18 or older, and "not old enough" if they aren't 18.
 var peopleWhoWantToSeeMadMaxFuryRoad = [
     {
         name: "Mike",
@@ -38,9 +38,8 @@ for(var i = 0; i < peopleWhoWantToSeeMadMaxFuryRoad.length; i++) {
     } 
 }
 
-// Log to the console a personalized message like:
-// Mike is not old enough to see Mad Max or Madeline is old enough to see Mad Max.
-
+// Log to the console a personalized message like:
+// Mike is not old enough to see Mad Max or Madeline is old enough to see Mad Max.
 for(var i = 0; i < peopleWhoWantToSeeMadMaxFuryRoad.length; i++) {    
     if(peopleWhoWantToSeeMadMaxFuryRoad[i].age >= 18){
        console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " Is Old Enough");       
@@ -53,7 +52,6 @@ for(var i = 0; i < peopleWhoWantToSeeMadMaxFuryRoad.length; i++) {
 //Check to see if the movie goer is a male or female for an even more personalized message.
 // Mike is not old enough to see Mad Max Fury Road, don't let HIM in or
 // Madeline is old enough. SHE'S good to see Mad Max Fury Road.
-
 for(var i = 0; i < peopleWhoWantToSeeMadMaxFuryRoad.length; i++) {    
     if(peopleWhoWantToSeeMadMaxFuryRoad[i].age >= 18){
        console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " Is Old Enough To See Mad Max Fury Road. " +
@@ -67,4 +65,10 @@ for(var i = 0; i < peopleWhoWantToSeeMadMaxFuryRoad.length; i++) {
 
 //Create a for loop that iterates through 101 numbers (from 0 - 100). 
 //If the current iteration is an Odd number, print "Odd" to the console, otherwise print "Even".
-
+for(var i = 0; i <= 101; i++) {
+    if(i % 2 === 1) {
+    console.log(i + " Is an odd number!");
+    } else {
+        console.log(i + " Is an even number");
+    }
+}
